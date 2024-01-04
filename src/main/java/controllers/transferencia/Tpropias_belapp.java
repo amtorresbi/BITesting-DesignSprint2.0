03871fc45a55f3725954db789095de347c232ea7
amtorresbi <amtorres@bi.com.gt>
2024-01-04 08:56:11 -0600

    Update ruta comando consola BrowserStack TP Bel App

--- src/main/java/controllers/transferencia/Tpropias_belapp.java
+++ src/main/java/controllers/transferencia/Tpropias_belapp.java
@@ -6,7 +6,7 @@ import java.io.IOException;
 public class Tpropias_belapp {
 
     public static void main(String[] args) throws IOException {
-        String ruta = System.getProperty("user.dir") + "\\BITesting-DesignSprint2.0\\";
+        String ruta = System.getProperty("user.dir") + "\\";
         ProcessBuilder pb = new ProcessBuilder();
         pb.directory(new File(ruta));
         pb.command("cmd", "/c","start", "cmd.exe", "/K", "mvn test -P TPropias-belapp");
